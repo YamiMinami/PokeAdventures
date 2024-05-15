7a42f4bbabe99abffb611fc55fc297397458c884
secci000 <s144124@ap.be>
2024-05-15 08:45:59 +0000

    volledig afgewerkt

--- public/scripts/script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("myUls");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
