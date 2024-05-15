d134ab024ed66e859a4f88457131f529302b64af
s117507 <s117507@ap.be>
2024-05-15 12:42:30 +0200

    implementations

--- index.ts
+++ index.ts
@@ -6,8 +6,14 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
     res.render("index");
 });
-app.get("/battle", (req, res) => {
-  res.render("battle");
+app.get("/battle", async (req, res) => {
+    const randomId = Math.floor(Math.random() * 898) + 1;
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
+    const pokemon = await response.json();
+
+  res.render("battle", {
+    pokemon: pokemon,
+  });
 });
 app.get("/compare", (req, res) => {
   res.render("compare");
@@ -44,18 +50,17 @@ app.get("/teamplanner", (req, res) => {
   res.render("teamplanner");
 });
 
+app.get("/menu", (req, res) => {
+  res.render("menu");
+});
+
 app.get("/detail/:id", async (req, res) => {
   const pokemonId = req.params.id;
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
   const data = await response.json();
   const pokemon = data;
-  // Voor matthew
-  const responseEvo = await fetch('https://pokeapi.co/api/v2/evolution-chain/${pokemonId}')
-  const dataEvo = await responseEvo.json();
-  const evolutions = dataEvo;
   res.render("detail", {
-    pokemon: pokemon,
-    evolutions: evolutions
+    pokemon: pokemon
   });
 });
 
