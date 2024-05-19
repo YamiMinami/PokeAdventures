6a54ec7b15a9b75130171e997f142059a49fcae2
s117507 <s117507@ap.be>
2024-05-19 21:14:13 +0000

    updated

--- index.ts
+++ index.ts
@@ -1,28 +1,45 @@
 import express from "express";
+import { MongoClient } from "mongodb";
+import dotenv from 'dotenv';
+import { Users } from "./interfaces";
 const app = express();
 app.set("view engine", "ejs");
 app.set("port", 3000);
 app.use(express.static("public"));
 
+const uri = "mongodb+srv://estalistrinev:tPqvaqEIdP7z9KM1@mijnproject.udzcq5y.mongodb.net/?retryWrites=true&w=majority&appName=mijnProject"
+const client = new MongoClient(uri);
 
+async function main() {
+  try {
+    await client.connect();
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+
+main();
 
 app.get("/", (req, res) => {
   res.render("index");
 });
-app.get("/battle", (req, res) => {
-  res.render("battle");
+app.get("/battle", async (req, res) => {
+    const randomId = Math.floor(Math.random() * 898) + 1;
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
+    const pokemon = await response.json();
+
+  res.render("battle", {
+    pokemon: pokemon,
+  });
 });
 app.get("/compare", (req, res) => {
   res.render("compare");
 });
 
-app.get("/guesspokemon", async(req, res) => {
-  let pokemon;
-  const randomId = Math.floor(Math.random() * 898) + 1;
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
-  const data = await response.json();
-  pokemon = data;
-  res.render("guesspokemon", {pokemon: pokemon});
+app.get("/guesspokemon", (req, res) => {
+  res.render("guesspokemon");
 });
 app.get("/inlog", (req, res) => {
   res.render("inlog");
@@ -45,12 +62,15 @@ app.get("/overzicht", async (req, res) => {
   });
 });
 
-
 app.get("/tester", (req, res) => {
   res.render("tester");
 });
 app.get("/teamplanner", (req, res) => {
   res.render("teamplanner");
+});
+
+app.get("/menu", (req, res) => {
+  res.render("menu");
 });
 
 app.get("/detail/:id", async (req, res) => {
@@ -69,5 +89,5 @@ app.get("/detail/:id", async (req, res) => {
 });
 
 app.listen(app.get("port"), () =>
-  console.log("[server] http://localhost:" + app.get("port"))
+    console.log("[server] http://localhost:" + app.get("port"))
 );
