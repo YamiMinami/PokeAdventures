e390d08ddef61698a5922f13d7e7df4e7fbd4512
s117507 <s117507@ap.be>
2024-05-29 16:03:01 +0200

    updated

--- public/scripts/toggleNav.js
+++ public/scripts/toggleNav.js
@@ -7,33 +7,3 @@ function toggleNav() {
       }
   }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const carouselTrack = document.querySelector('.carousel-track');
-    const carouselItems = Array.from(document.querySelectorAll('.carousel-item'));
-    const scrollUpButton = document.getElementById('scroll-up');
-    const scrollDownButton = document.getElementById('scroll-down');
-
-    let currentIndex = 0;
-    const maxVisibleItems = 4;
-
-    updateCarousel();
-
-    scrollUpButton.addEventListener('click', () => {
-        if (currentIndex > 0) {
-            currentIndex--;
-            updateCarousel();
-        }
-    });
-
-    scrollDownButton.addEventListener('click', () => {
-        if (currentIndex < carouselItems.length - maxVisibleItems) {
-            currentIndex++;
-            updateCarousel();
-        }
-    });
-
-    function updateCarousel() {
-        const offset = currentIndex * (carouselItems[0].offsetHeight + 20);
-        carouselTrack.style.transform = `translateY(-${offset}px)`;
-    }
-});
